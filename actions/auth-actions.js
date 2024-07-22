20f6664c4068fc1ddcb8fe1579ef746922f8b8ca
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-07-22 12:48:25 +0200

    feat: Verify an Active Auth Session

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -5,35 +5,35 @@ import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
 import { redirect } from "next/navigation";
 
-export async function signup(_prevState, formData) {
-  const email = formData.get('email');
-  const password = formData.get('password');
+export async function signup( _prevState, formData ) {
+  const email = formData.get( 'email' );
+  const password = formData.get( 'password' );
 
   let errors = {};
 
-  if (!email.includes('@')) {
+  if ( !email.includes( '@' ) ) {
     errors.email = 'Email is invalid';
   }
 
-  if (password.trim().length < 8) {
+  if ( password.trim().length < 8 ) {
     errors.password = 'Password must be at least 8 characters long';
   }
 
-  if (Object.keys(errors).length > 0) {
+  if ( Object.keys( errors ).length > 0 ) {
     return { errors };
   }
 
   // * INFO: Create new user in DB w/ secured hashed password!
-  const hashedPassword = hashUserPassword(password);
+  const hashedPassword = hashUserPassword( password );
   try {
-    const id = createUser(email, hashedPassword)
+    const id = createUser( email, hashedPassword )
 
     // * INFO: Create new session in DB
-    await createAuthSession(id);
-    redirect('/training')
+    await createAuthSession( id );
+    redirect( '/training' )
 
-  } catch (error) {
-    if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+  } catch ( error ) {
+    if ( error.code === 'SQLITE_CONSTRAINT_UNIQUE' ) {
       return {
         errors:
           { email: 'Email already exists' }
@@ -43,3 +43,5 @@ export async function signup(_prevState, formData) {
   }
 
 }
+
+
